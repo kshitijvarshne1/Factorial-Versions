31681c5383177892b27a101f1d73ba1dd02fe85c
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-18 00:23:15 +0530

    create FactorialCalculationUsingMultithreading.java

---
+++ src/FactorialCalculationUsingMultithreading.java
@@ -0,0 +1,10 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 18-Apr-21
+ *   Time: 12:23 AM
+ *   File: FactorialCalculationUsingMultithreading.java
+ */
+
+public class FactorialCalculationUsingMultithreading {
+}
+
