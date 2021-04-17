3ea852579b5cdfafb8ab5e1450d465c857572ca9
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-18 00:16:49 +0530

    create FactorialCalculationSimply.java

---
+++ src/FactorialCalculationSimply.java
@@ -0,0 +1,10 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 18-Apr-21
+ *   Time: 12:16 AM
+ *   File: FactorialCalculationSimply.java
+ */
+
+public class FactorialCalculationSimply {
+}
+
