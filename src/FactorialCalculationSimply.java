bd255d93938b203c08e285e1d563f2d98c8d10eb
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-18 00:22:15 +0530

    implement the calculation method

--- src/FactorialCalculationSimply.java
+++ src/FactorialCalculationSimply.java
@@ -9,14 +9,20 @@ import java.math.BigInteger;
 
 public class FactorialCalculationSimply {
     public static void main(String[] args) {
-
+        long start = System.currentTimeMillis();
+        int [] arr= {1000,2000,3000,4000,5000};
+        for (int i = 0; i < arr.length; i++) {
+            calculation(arr[i]);
+        }
+        System.out.println(System.currentTimeMillis()-start);
     }
-    private void calculation(int n){
+    private static void calculation(int n){
         BigInteger result= BigInteger.ONE;
         for (int i = 2; i <=n; i++) {
             result=result.multiply(BigInteger.valueOf(i));
         }
         System.out.println(result);
     }
+
 }
 
