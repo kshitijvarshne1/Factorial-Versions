dc56f33b2d176823585ae1c7ffa383f992cae771
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-18 00:19:22 +0530

    add calculation method

--- src/FactorialCalculationSimply.java
+++ src/FactorialCalculationSimply.java
@@ -5,6 +5,18 @@
  *   File: FactorialCalculationSimply.java
  */
 
+import java.math.BigInteger;
+
 public class FactorialCalculationSimply {
+    public static void main(String[] args) {
+
+    }
+    private void calculation(int n){
+        BigInteger result= BigInteger.ONE;
+        for (int i = 2; i <=n; i++) {
+            result=result.multiply(BigInteger.valueOf(i));
+        }
+        System.out.println(result);
+    }
 }
 
